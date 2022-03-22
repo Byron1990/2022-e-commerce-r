592d0e8100993995d0cd742e0f7b8093ed7a80e2
Byron <byron.mena.7@gmail.com>
2022-03-22 04:57:47 -0500

    Card of product
    The card defines de product name, price, picture and a small description.

--- src/components/Product.js
+++ src/components/Product.js
@@ -6,18 +6,37 @@ import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
 import CardActions from "@mui/material/CardActions";
 import Collapse from "@mui/material/Collapse";
-import Avatar from "@mui/material/Avatar";
 import IconButton, { IconButtonProps } from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
-import { red } from "@mui/material/colors";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import ShareIcon from "@mui/icons-material/Share";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import MoreVertIcon from "@mui/icons-material/MoreVert";
-
+import { AddShoppingCart } from "@material-ui/icons";
+import accounting from "accounting";
 interface ExpandMoreProps extends IconButtonProps {
   expand: boolean;
 }
+
+/* const useStyle = makeStyles(() => ({
+  root: {
+    maxWidth: 345,
+  },
+  action: {
+    marginTop: "1rem",
+  },
+  media: {
+    height: 0,
+    paddingTop: "56.25%", // 16:9
+  },
+  expand: {
+    transform: "rotate(0deg)",
+    marginLeft: "auto",
+    transition: theme.transitions.create("transform", {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: "rotate(180deg)",
+  },
+})); */
 
 const ExpandMore = styled((props: ExpandMoreProps) => {
   const { expand, ...other } = props;
@@ -36,42 +55,38 @@ export default function Product() {
   const handleExpandClick = () => {
     setExpanded(!expanded);
   };
-
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardHeader
-        /* avatar={
-          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-            R
-          </Avatar>
-        } */
         action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
+          <Typography variant="h5" color="textSecondary">
+            {accounting.formatMoney(4999.99)}
+          </Typography>
         }
-        title="Shrimp and Chorizo Paella"
-        subheader="September 14, 2016"
+        title="Shoes"
+        subheader="in Stock"
       />
       <CardMedia
         component="img"
         height="194"
-        image="/static/images/cards/paella.jpg"
+        image="https://www.popsci.com/uploads/2021/01/14/54BGRKCC7BEAPGZ3XAK5RHJF6M.jpg?auto=webp"
         alt="Paella dish"
       />
       <CardContent>
         <Typography variant="body2" color="text.secondary">
-          This impressive paella is a perfect party dish and a fun meal to cook
-          together with your guests. Add 1 cup of frozen peas along with the
-          mussels, if you like.
+          Running shoes
         </Typography>
       </CardContent>
       <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
+        <IconButton aria-label="Add to Cart">
+          <AddShoppingCart font-size="lange" />
         </IconButton>
-        <IconButton aria-label="share">
-          <ShareIcon />
+        <IconButton>
+          {Array(4)
+            .fill()
+            .map((_, i) => (
+              <p>&#x2B50;</p>
+            ))}
         </IconButton>
         <ExpandMore
           expand={expanded}
@@ -84,32 +99,8 @@ export default function Product() {
       </CardActions>
       <Collapse in={expanded} timeout="auto" unmountOnExit>
         <CardContent>
-          <Typography paragraph>Method:</Typography>
           <Typography paragraph>
-            Heat 1/2 cup of the broth in a pot until simmering, add saffron and
-            set aside for 10 minutes.
-          </Typography>
-          <Typography paragraph>
-            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet
-            over medium-high heat. Add chicken, shrimp and chorizo, and cook,
-            stirring occasionally until lightly browned, 6 to 8 minutes.
-            Transfer shrimp to a large plate and set aside, leaving chicken and
-            chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes,
-            onion, salt and pepper, and cook, stirring often until thickened and
-            fragrant, about 10 minutes. Add saffron broth and remaining 4 1/2
-            cups chicken broth; bring to a boil.
-          </Typography>
-          <Typography paragraph>
-            Add rice and stir very gently to distribute. Top with artichokes and
-            peppers, and cook without stirring, until most of the liquid is
-            absorbed, 15 to 18 minutes. Reduce heat to medium-low, add reserved
-            shrimp and mussels, tucking them down into the rice, and cook again
-            without stirring, until mussels have opened and rice is just tender,
-            5 to 7 minutes more. (Discard any mussels that don’t open.)
-          </Typography>
-          <Typography>
-            Set aside off of the heat to let rest for 10 minutes, and then
-            serve.
+            "Running shoes fot the tiger that is inside you"
           </Typography>
         </CardContent>
       </Collapse>
---
+++ src/product-data.js
@@ -0,0 +1,34 @@
+const products = [
+  {
+    id: 1,
+    name: "Shoes",
+    productType: "Running shoes",
+    price: "100",
+    rating: 4,
+    image:
+      "https://www.popsci.com/uploads/2021/01/14/54BGRKCC7BEAPGZ3XAK5RHJF6M.jpg?auto=webp",
+    description: "Running shoes for the tiger that live in you",
+  },
+  {
+    id: 2,
+    name: "Auto tank",
+    productType: "Toys",
+    price: "150",
+    rating: 4,
+    image:
+      "https://http2.mlstatic.com/D_NQ_NP_625465-MCO48529256516_122021-O.webp",
+    description:
+      "Auto tank that can be controlled by hand, rotates 360 degress and shoot gummy bullets",
+  },
+  {
+    id: 3,
+    name: "Drone",
+    productType: "Toys",
+    price: "170",
+    rating: 5,
+    image:
+      "https://ae01.alicdn.com/kf/Ha094fe9e3dc64df99c841d35550b33c2c/Dron-plegable-con-WIFI-Avi-n-de-tiro-a-reo-de-altura-fija-control-remoto-juguete.jpg_Q90.jpg_.webp",
+    description:
+      "Drone with 4k camera, time to fly around 20 minutes and can be controlled until 200 meters",
+  },
+];
